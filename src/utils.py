98c6681f075ecf0ec12eeb4922e8d32e6d4a84e4
Emulie Chhor <emulie.chhor@hotmail.com>
2024-01-29 17:08:52 -0500

    using supabase secret token instead of public token

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ from supabase import create_client, Client
 def load_supabase_client() -> Client:
     dotenv.load_dotenv()
     url = os.environ.get("SUPABASE_URL")
-    key = os.environ.get("SUPABASE_KEY")
+    key = os.environ.get("SUPABASE_SECRET_KEY")
     try: 
         client = create_client(supabase_url=url, supabase_key=key)
         logging.info("Successfully connected to supabase client")
